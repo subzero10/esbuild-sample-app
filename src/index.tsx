42ce6e21749c4cd83e89f28f2680ec2786b18d00
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2024-09-19 09:20:29 +0300

    chore: hb testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { App } from "./app";
+import App from "./App";
 import "./index.css";
 
 import { Honeybadger, HoneybadgerErrorBoundary } from '@honeybadger-io/react'
